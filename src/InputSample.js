c2a96cf604c3d23a83d14849d5873001e8fdbcb5
Alex <dnjscksdn98@gmail.com>
2020-01-13 22:47:28 +0900

    Update file

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,54 @@
 import React, { useState } from "react";
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: '',
+    });
+    const { name, nickname } = inputs;
+
     const onChange = (e) => {
-        // e.target : 현재 DOM 을 가리킴
-        setText(e.target.value);
-    }
+        const { name, value } = e.target;  // name 과 value 를 추출
+
+        // 불변성을 지킨다
+        // 객체 상태를 업데이트 할 때
+        // 기존의 객체를 복사
+        // const nextInputs = {
+        //     ...inputs,
+        //     [name]: value,  // if name="name" change name, if name="nickname" change nickname
+        // };
+        // 그 후에 덮어 쓴다
+        // setInputs(nextInputs);
+        setInputs({
+            ...inputs,  // spread 로 복사
+            [name]: value,
+        });
+    };
     const onReset = () => {
-        setText('');
-    }
+        setInputs({
+            name: '',
+            nickname: '',
+        });
+    };
 
     return (
         <div>
-            <input onChange={onChange} value={text} />
+            <input
+                name="name"
+                placeholder="이름"
+                onChange={onChange}
+                value={name}
+            />
+            <input
+                name="nickname"
+                placeholder="닉네임"
+                onChange={onChange}
+                value={nickname}
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                {text}
+                {name}({nickname})
             </div>
         </div>
     );
