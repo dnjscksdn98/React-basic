5eeb29fdb1478fa11eede673013a4ac89c20870a
Alex <dnjscksdn98@gmail.com>
2020-01-15 20:45:39 +0900

    Add React.memo

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,23 +1,24 @@
-import React from 'react';
-
+import React from "react";
 
 function CreateUser({ username, email, onChange, onCreate }) {
-    return (
-        <div>
-            <input
-                name='username'
-                placeholder='계정명'
-                onChange={onChange}
-                value={username} />
-            <input
-                name='email'
-                placeholder='이메일'
-                onChange={onChange}
-                value={email}
-            />
-            <button onClick={onCreate}>등록</button>
-        </div>
-    );
+  return (
+    <div>
+      <input
+        name="username"
+        placeholder="계정명"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
 }
 
-export default CreateUser;
+// React.memo: props 가 바뀔때만 리렌더링 한다
+export default React.memo(CreateUser);
