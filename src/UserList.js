0b18c5bf47f8377562f2eb81b3c1cc56f7fb0e5e
Alex <dnjscksdn98@gmail.com>
2020-01-15 21:24:35 +0900

    Use the context and dispatch

--- src/UserList.js
+++ src/UserList.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserDispatch } from "./App";
 
 // 컴포넌트를 하나 더 생성
-const User = React.memo(function User({ user, onRemove, onToggle }) {
+const User = React.memo(function User({ user }) {
   const { username, email, id, active } = user;
 
-  // deps 를 지정하면 값이 변경 또는 설정될 때마다 호출이됨, 처음 나타낼때에도 호출이됨
-  // useEffect(() => {
-  // }, [user]);
+  // use the context
+  const dispatch = useContext(UserDispatch);
 
   return (
     <div>
@@ -15,29 +15,37 @@ const User = React.memo(function User({ user, onRemove, onToggle }) {
           color: active ? "green" : "black",
           cursor: "pointer"
         }}
-        onClick={() => onToggle(id)} // don't call a function, make a function
+        onClick={() =>
+          dispatch({
+            type: "TOGGLE_USER",
+            id
+          })
+        }
       >
         {username}
       </b>
       <span>({email})</span>
-      <button onClick={() => onRemove(id)}>삭제</button>{" "}
-      {/* 함수를 호출하는 것을 넣으면 안되고 함수 자체를 넣어야함 */}
+      <button
+        onClick={() =>
+          dispatch({
+            type: "REMOVE_USER",
+            id
+          })
+        }
+      >
+        삭제
+      </button>{" "}
     </div>
   );
 });
 
-function UserList({ users, onRemove, onToggle }) {
+function UserList({ users }) {
   return (
     <div>
       {users.map(
         // 키를 지정해놔야 효율적으로 렌더링을 할 수 있다
         user => (
-          <User
-            user={user}
-            key={user.id}
-            onRemove={onRemove}
-            onToggle={onToggle}
-          />
+          <User user={user} key={user.id} />
         ) // 각자의 고유한 키가 필요하므로 key 값을 id 로 지정
       )}
     </div>
