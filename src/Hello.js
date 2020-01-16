b19b7a6661c6c97c9ead761835f162f90617dd4d
Alex <dnjscksdn98@gmail.com>
2020-01-16 17:05:01 +0900

    Remake Hello component to a class component

--- src/Hello.js
+++ src/Hello.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { Component } from "react";
 
-// 컴포넌트 선언 - 함수
-function Hello({ color, name, isSpecial }) {
+// class component
+class Hello extends Component {
+  // class component - default props
+  static defaultProps = {
+    name: "no-name"
+  };
+
+  render() {
+    const { color, isSpecial, name } = this.props;
+
     return (
-        <div style={{ color }}>
-            {/* {isSpecial ? <b>*</b> : null} */}
-            {isSpecial && <b>*</b>}
-            안녕하세요 {name}
-        </div>
+      <div style={{ color }}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+      </div>
     );
+  }
 }
 
-// 기본값 지정
-Hello.defaultProps = {
-    name: "javascript"
-}
+// // function component
+// function Hello({ color, name, isSpecial }) {
+//     return (
+//         <div style={{ color }}>
+//             {/* {isSpecial ? <b>*</b> : null} */}
+//             {isSpecial && <b>*</b>}
+//             안녕하세요 {name}
+//         </div>
+//     );
+// }
 
-export default Hello;  // Hello 컴포넌트를 내보낸다
+// // function component - default props
+// Hello.defaultProps = {
+//   name: "javascript"
+// };
+
+export default Hello;
